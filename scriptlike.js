12a826801821233c9101ee0ec2fcc2d5cf413f3f
Premal upadhyay <34483490+premaluu@users.noreply.github.com>
2020-03-06 15:45:57 +0530

    First commit

---
+++ scriptlike.js
@@ -0,0 +1,13 @@
+$('.btnlike-counter').on('click', function(event, count) {
+  event.preventDefault();
+  
+  var $this = $(this),
+      count = $this.attr('data-count'),
+      active = $this.hasClass('active'),
+      multiple = $this.hasClass('multiple-count');
+
+  
+  $.fn.noop = $.noop;
+  $this.attr('data-count', ! active || multiple ? ++count : --count  )[multiple ? 'noop' : 'toggleClass']('active');
+  
+});
